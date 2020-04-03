0ff44218e9c28f451cd4f24b48292759216ea426
yotamc <39822059+yotamc@users.noreply.github.com>
2020-04-03 12:35:38 +0300

    add mouse event blocking

--- DoubleClickFix/InterceptMouse.cs
+++ DoubleClickFix/InterceptMouse.cs
@@ -1,11 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
 
 namespace DoubleClickFix
 {
     class InterceptMouse
     {
-        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+        private const int MinTimeDifference = 125;
+        private delegate IntPtr LowLevelMouseProc(int nCode, MouseMessages wParam, IntPtr lParam);
+
+        private Dictionary<MouseMessages, uint> _lastEventTimes = new Dictionary<MouseMessages, uint>();
 
         private LowLevelMouseProc _proc;
         private IntPtr _hookID = IntPtr.Zero;
@@ -13,17 +18,19 @@ namespace DoubleClickFix
         public InterceptMouse()
         {
             _proc = HookCallback;
+
+            //TODO make this configurable
+            _lastEventTimes[MouseMessages.WM_LBUTTONDOWN] = 0;
+            _lastEventTimes[MouseMessages.WM_LBUTTONUP] = 0;
         }
 
         public void Hook()
         {
-            Console.WriteLine("hooking");
             _hookID = SetHook(_proc);
         }
 
         public void Unhook()
         {
-            Console.WriteLine("unhooking");
             UnhookWindowsHookEx(_hookID);
         }
 
@@ -32,7 +39,7 @@ namespace DoubleClickFix
             IntPtr hook = SetWindowsHookEx(HookType.WH_MOUSE_LL, proc, GetModuleHandle("user32"), 0);
             if (hook == IntPtr.Zero)
             {
-                throw new System.ComponentModel.Win32Exception();
+                throw new Win32Exception();
             }
             return hook;
             // using (Process curProcess = Process.GetCurrentProcess())
@@ -43,30 +50,37 @@ namespace DoubleClickFix
             // }
         }
 
-
-        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        private IntPtr HookCallback(int nCode, MouseMessages wParam, IntPtr lParam)
         {
-            if (nCode >= 0 &&
-                MouseMessages.WM_LBUTTONDOWN == (MouseMessages)wParam)
+            if (nCode >= 0 && _lastEventTimes.ContainsKey(wParam))
             {
                 MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
-                Console.WriteLine(hookStruct.pt.x + ", " + hookStruct.pt.y);
+
+                var timeDiff = hookStruct.time - _lastEventTimes[wParam];
+                var shouldBlock = timeDiff < MinTimeDifference;
+                Console.WriteLine($"{hookStruct.time} (d:{timeDiff}ms) {wParam} {(shouldBlock ? "BLOCKED" : "OK")}: x:{hookStruct.pt.x} y:{hookStruct.pt.y}");
+                if (shouldBlock)
+                {
+                    return new IntPtr(1);
+                }
+
+                _lastEventTimes[wParam] = hookStruct.time;
             }
             return CallNextHookEx(_hookID, nCode, wParam, lParam);
         }
 
         private enum HookType : int
         {
-            WH_MOUSE = 7,
             WH_MOUSE_LL = 14
         }
 
-        private enum MouseMessages
+        private enum MouseMessages : int
         {
             WM_LBUTTONDOWN = 0x0201,
             WM_LBUTTONUP = 0x0202,
             WM_MOUSEMOVE = 0x0200,
             WM_MOUSEWHEEL = 0x020A,
+            WM_MOUSEHWHEEL = 0x020E,
             WM_RBUTTONDOWN = 0x0204,
             WM_RBUTTONUP = 0x0205
         }
@@ -98,7 +112,7 @@ namespace DoubleClickFix
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
-            IntPtr wParam, IntPtr lParam);
+            MouseMessages wParam, IntPtr lParam);
 
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr GetModuleHandle(string lpModuleName);
