a487a5ce1c41c0dc5e6cf765edde33a8844347bb
yotamc <39822059+yotamc@users.noreply.github.com>
2020-06-17 11:36:11 +0300

    update

---
+++ DoubleClickFix/Configuration/WritableJsonConfigurationProvider.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (C) 2020 Yotam Cohen
+//
+// This file is part of DoubleClickFix.
+//
+// DoubleClickFix is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// DoubleClickFix is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with DoubleClickFix.  If not, see <http://www.gnu.org/licenses/>.
+
+using Microsoft.Extensions.Configuration.Json;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace DoubleClickFix.Configuration
+{
+    public class WritableJsonConfigurationProvider : JsonConfigurationProvider
+    {
+        public WritableJsonConfigurationProvider(WritableJsonConfigurationSource source) : base(source)
+        {
+        }
+
+        public override void Set(string key, string value)
+        {
+            base.Set(key, value);
+
+            var fileFullPath = Source.FileProvider.GetFileInfo(Source.Path).PhysicalPath;
+            string json = File.ReadAllText(fileFullPath);
+            dynamic jsonObj = JsonConvert.DeserializeObject(json);
+            jsonObj[key] = value;
+            string output = JsonConvert.SerializeObject(jsonObj, Formatting.Indented);
+            File.WriteAllText(fileFullPath, output);
+        }
+    }
+}
--- DoubleClickFix/Configuration/WriteableJsonConfigurationExtensions.cs
+++ DoubleClickFix/Configuration/WriteableJsonConfigurationExtensions.cs
@@ -16,41 +16,12 @@
 // along with DoubleClickFix.  If not, see <http://www.gnu.org/licenses/>.
 
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.FileProviders;
-using Newtonsoft.Json;
 using System;
 using System.IO;
 
 namespace DoubleClickFix.Configuration
 {
-    public class WritableJsonConfigurationProvider : JsonConfigurationProvider
-    {
-        public WritableJsonConfigurationProvider(WritableJsonConfigurationSource source) : base(source)
-        {
-        }
-
-        public override void Set(string key, string value)
-        {
-            base.Set(key, value);
-
-            var fileFullPath = Source.FileProvider.GetFileInfo(Source.Path).PhysicalPath;
-            string json = File.ReadAllText(fileFullPath);
-            dynamic jsonObj = JsonConvert.DeserializeObject(json);
-            jsonObj[key] = value;
-            string output = JsonConvert.SerializeObject(jsonObj, Formatting.Indented);
-            File.WriteAllText(fileFullPath, output);
-        }
-    }
-
-    public class WritableJsonConfigurationSource : JsonConfigurationSource
-    {
-        public override IConfigurationProvider Build(IConfigurationBuilder builder)
-        {
-            EnsureDefaults(builder);
-            return new WritableJsonConfigurationProvider(this);
-        }
-    }
 
     public static class WriteableJsonConfigurationExtensions
     {
